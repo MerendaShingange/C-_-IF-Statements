70373b51a43779502c68d40bf812e4a96f2f2d84
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 20:10:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___IF_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // the test can be either true or false
+            bool isMale = true;
+            if (isMale )
+            {
+                Console.WriteLine("You are male ");
+
+            }
+            else
+            {
+                Console.WriteLine("You are female");
+                
+                
+            }
+            bool isTall = false;
+            if (isTall&& isMale  ) {
+            Console.WriteLine("You are not tall but you are male");
+
+            }
+            else if(isTall&& isMale)
+            {
+                Console.WriteLine("You are not tall but you are male");
+
+            }
+            else { Console.WriteLine("You are either tall, not tall or not a mele");
+            }
+        }
+    }
+}
